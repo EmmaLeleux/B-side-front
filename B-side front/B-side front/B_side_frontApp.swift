68b32f85911d63150f48c95c929c49965edddad6
Emma Leleux <leleux.emma@gmail.com>
2025-12-10 14:16:43 +0100

    welcome to the b-siiiiiiiide

---
+++ B-side front/B-side front/B_side_frontApp.swift
@@ -0,0 +1,17 @@
+//
+//  B_side_frontApp.swift
+//  B-side front
+//
+//  Created by Emma on 10/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct B_side_frontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ B-side front/B-side front/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  B-side front
+//
+//  Created by Emma on 10/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome to the B-Siiiiiiiide")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
